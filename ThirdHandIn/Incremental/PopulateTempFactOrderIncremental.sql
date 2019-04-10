453e24eb808ac5a704be265702725351a02d9878
vidwalk <claudiurediu@gmail.com>
2019-04-10 20:09:12 +0200

    Update on Populating the temporary fact order

--- ThirdHandIn/Incremental/PopulateTempFactOrderIncremental.sql
+++ ThirdHandIn/Incremental/PopulateTempFactOrderIncremental.sql
@@ -2,7 +2,7 @@ use NorthWindDB
 go
 DELETE FROM [OrderSaleProcess].[dbo].[temp_fact_order]
 go
----Insert into the Temporary Fact Order the entries up until 12/31/1997 in the Order Details
+---Insert into the Temporary Fact Order the entries after 12/31/1997 in the Order Details
 insert into [OrderSaleProcess].[dbo].[temp_fact_order](Quantity,SalesAmount,ProductID,CustomerID,EmployeeID,OrderDate,Discount)
 SELECT
 Quantity
@@ -17,9 +17,10 @@ Join Orders on Orders.OrderID = [Order Details].OrderID
 Join Customers on Orders.CustomerID=Customers.CustomerID
 Join Employees on Employees.EmployeeID= Orders.EmployeeID
 Join Products on Products.ProductID= [Order Details].ProductID
---Retrieve the order up until 12/31/1997
+--Retrieve the orders after 31/01/1997
 WHERE [Orders].OrderDate > (SELECT MAX(CurrentValid) FROM OrderSaleProcess.dbo.Validator)
 --Insert the value of the latest read into the Validator table which is made to keep track of the latest validTo
 INSERT INTO OrderSaleProcess.dbo.Validator
 (CurrentValid)
 VALUES(DATEADD(DAY, 1, (SELECT MAX(CurrentValid) FROM OrderSaleProcess.dbo.Validator)));
+---This has added 01/01/1998 to the validator
